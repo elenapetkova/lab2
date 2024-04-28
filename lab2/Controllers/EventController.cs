c6d52c8c99854fc6b2aa0f16c283731da1e4f830
Вашето Име <elena.petkova@students.finki.ukim.com>
2024-04-29 01:24:51 +0200

    Add project files.

---
+++ lab2/Controllers/EventController.cs
@@ -0,0 +1,121 @@
+﻿using lab2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab2.Controllers
+{
+    
+        public class EventController : Controller
+        {
+            private static List<EventModel> events = new List<EventModel>();
+
+            // GET: Event
+            public ActionResult Index()
+            {
+                return View(events);
+            }
+
+            // GET: Event/Details/5
+            public ActionResult Details(int? id)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                EventModel eventModel = events.FirstOrDefault(e => e.Id == id);
+                if (eventModel == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(eventModel);
+            }
+            [HttpPost]
+            public ActionResult Update(EventModel model)
+            {
+                EventModel existingEvent = events.FirstOrDefault(e => e.Id == model.Id);
+                if (existingEvent == null)
+                {
+                    return HttpNotFound();
+                }
+                existingEvent.Ime = model.Ime;
+                existingEvent.Lokacija = model.Lokacija;
+                return RedirectToAction("Details", new { id = existingEvent.Id });
+            }
+
+            // GET: Event/Create
+            public ActionResult Create()
+            {
+                return View();
+            }
+
+            // POST: Event/Create
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public ActionResult Create(EventModel eventModel)
+            {
+                if (ModelState.IsValid)
+                {
+                    events.Add(eventModel);
+                    return RedirectToAction("Index");
+                }
+
+                return View(eventModel);
+            }
+
+            // GET: Event/Edit/5
+            public ActionResult Edit(int? id)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                EventModel eventModel = events.FirstOrDefault(e => e.Id == id);
+                if (eventModel == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(eventModel);
+            }
+
+            // POST: Event/Edit/5
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public ActionResult Edit(EventModel eventModel)
+            {
+                if (ModelState.IsValid)
+                {
+                    return RedirectToAction("Index");
+                }
+                return View(eventModel);
+            }
+
+            // GET: Event/Delete/5
+            public ActionResult Delete(int? id)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                EventModel eventModel = events.FirstOrDefault(e => e.Id == id);
+                if (eventModel == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(eventModel);
+            }
+
+            // POST: Event/Delete/5
+            [HttpPost, ActionName("Delete")]
+            [ValidateAntiForgeryToken]
+            public ActionResult DeleteConfirmed(int id)
+            {
+                EventModel eventModel = events.FirstOrDefault(e => e.Id == id);
+                events.Remove(eventModel);
+                return RedirectToAction("Index");
+            }
+        }
+    }
---
+++ lab2/Models/EventModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace lab2.Models
+{
+    public class EventModel
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        [StringLength(30, MinimumLength = 5)]
+        public string Lokacija { get; set; }
+    }
+}
